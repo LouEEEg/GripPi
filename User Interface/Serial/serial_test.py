a48f1f1ef533ff9a86cce3cb56ca7d20814e686f
Vincent9341 <114629726+Vincent9341@users.noreply.github.com>
2022-11-05 12:31:41 -0700

    Update serial_test.py

--- User Interface/Serial/serial_test.py
+++ User Interface/Serial/serial_test.py
@@ -3,6 +3,7 @@ import serial
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
+    ser.write(str(21).encode('utf-8'))
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
