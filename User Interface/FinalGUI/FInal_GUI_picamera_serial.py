ab5cc42a65c42f446c45cb4d8dbd924a2a0b9b05
Vincent9341 <114629726+Vincent9341@users.noreply.github.com>
2022-11-23 16:57:55 -0800

    Update FInal_GUI_picamera_serial.py

--- User Interface/FinalGUI/FInal_GUI_picamera_serial.py
+++ User Interface/FinalGUI/FInal_GUI_picamera_serial.py
@@ -8,12 +8,13 @@ from picamera2 import Picamera2, Preview
 
 
 #RESOLUTION = "1920x1080"
-RESOLUTION = "1024x600"
+RESOLUTION = "1024x768"
 
 root=Tk()
 root.title("GripPi")
 root.geometry(RESOLUTION)
 root["bg"]="#0066FC"
+root.attributes('-fullscreen', True)
 
 config = ConfigParser()
 config.read('config.ini')
@@ -29,7 +30,7 @@ photo1 = PhotoImage(file = "bin1.png")
 photo2 = PhotoImage(file = "bin2.png")
 photo3 = PhotoImage(file = "bin3.png")
 #background_image = PhotoImage(file = "GripPi.png")
-background_image = PhotoImage(file = "GripPi_small.png")
+#background_image = PhotoImage(file = "GripPi_small.png")
 #empty_bin_image = PhotoImage(file = "EmptyBin.png")
 empty_bin_image = PhotoImage(file = "EmptyBin_small.png")
 
@@ -62,6 +63,9 @@ bin3 = gripPiBin(config.getboolean('main', 'bin3_isEmpty'), bin3_image)
 def close_gui():
     sys.exit()
 
+def calibrate():
+    homeSet = GripPiSerial(99)
+
 def screenHome():
     
     
@@ -73,15 +77,15 @@ def screenHome():
         clearWorkspaceLabel.destroy()
         powerButton.destroy()
         calibrateButton.destroy()
-        motionControlButton.destroy()
+        #motionControlButton.destroy()
+        binsButton.destroy()
 
         #Top of page 2
         label21=Label(root, text="Select an Item", font=("Times_New_Roman",25), anchor="e", justify=CENTER)
         label21.pack(side=TOP)
         
                 
-        homeSet = GripPiSerial(99)
-          
+        #homeSet = GripPiSerial(99)
         
         def back():
             label21.destroy()
@@ -93,7 +97,7 @@ def screenHome():
             
         
         
-        backButton = Button(root,text="BACK",font=("Times_New_Roman",25),command=back,activebackground="red")
+        backButton = Button(root,text="BACK",font=("Times_New_Roman",25),command=lambda: [back()],activebackground="red")
         backButton.pack(side=BOTTOM)
         
         itemButtonLeft = Button(root,image=bin1.image,command=lambda: [updateItemButtonLeft()],activebackground="red", justify=LEFT)
@@ -181,78 +185,79 @@ def screenHome():
     # --- --- --- --- --- --- --- Motion Control Screen - --- --- --- --- --- --- --- 
     # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
 
-    def screenMotionControl():
-        homeLabel.destroy()
-        clearWorkspaceLabel.destroy()
-        powerButton.destroy()
-        calibrateButton.destroy()
-        motionControlButton.destroy()
+    #def screenMotionControl():
+        #homeLabel.destroy()
+        #clearWorkspaceLabel.destroy()
+        #powerButton.destroy()
+        #calibrateButton.destroy()
+        #motionControlButton.destroy()
+        #binsButton.destroy()
         
         
-        background_label = Label(root, image=background_image)
-        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        #background_label = Label(root, image=background_image)
+        #background_label.place(x=0, y=0, relwidth=1, relheight=1)
         
-        def back():
-            backButton.destroy()
+        #def back():
+            #backButton.destroy()
             #scale.destroy()
             #button.destroy()
             #scale2.destroy()
             #button2.destroy()
-            background_label.destroy()
+            #background_label.destroy()
             #label.destroy()
-            joint1.destroy()
-            joint2.destroy()
-            joint3.destroy()
-            joint4.destroy()
-            joint5.destroy()
-            e_1.destroy()
-            e_2.destroy()
-            e_3.destroy()
-            e_4.destroy()
-            e_5.destroy()
-            joint1data.destroy()
-            joint2data.destroy()
-            joint3data.destroy()
-            joint4data.destroy()
-            joint5data.destroy()
-            screenHome()
+            #joint1.destroy()
+            #joint2.destroy()
+            #joint3.destroy()
+            #joint4.destroy()
+            #joint5.destroy()
+            #e_1.destroy()
+            #e_2.destroy()
+            #e_3.destroy()
+            #e_4.destroy()
+            #e_5.destroy()
+            #joint1data.destroy()
+            #joint2data.destroy()
+            #joint3data.destroy()
+            #joint4data.destroy()
+            #joint5data.destroy()
+            #screenHome()
         
-        backButton = Button(root,text="BACK",font=("Times_New_Roman",25),command=back,activebackground="red")
-        backButton.pack(side=BOTTOM)
+        #backButton = Button(root,text="BACK",font=("Times_New_Roman",25),command=lambda: [back()],activebackground="red")
+        #backButton.pack(side=BOTTOM)
         
         # --- --- Input Box --- ---
-        joint1 = Label(root, text = "Joint 1 Position")
-        joint1.place(relx=0.75, rely=0.5 + 0.05)
-        joint2 = Label(root, text = "Joint 2 Position")
-        joint2.place(relx=0.75, rely=0.5 + 0.10)
-        joint3 = Label(root, text = "Joint 3 Position")
-        joint3.place(relx=0.75, rely=0.5 + 0.15)
-        joint4 = Label(root, text = "Joint 4 Position")
-        joint4.place(relx=0.75, rely=0.5 + 0.20)
-        joint5 = Label(root, text = "Joint 5 Position")
-        joint5.place(relx=0.75, rely=0.5 + 0.25)
+        #joint1 = Label(root, text = "Joint 1 Position")
+        #joint1.place(relx=0.75, rely=0.5 + 0.05)
+        #joint2 = Label(root, text = "Joint 2 Position")
+        #joint2.place(relx=0.75, rely=0.5 + 0.10)
+        #joint3 = Label(root, text = "Joint 3 Position")
+        #joint3.place(relx=0.75, rely=0.5 + 0.15)
+        #joint4 = Label(root, text = "Joint 4 Position")
+        #joint4.place(relx=0.75, rely=0.5 + 0.20)
+        #joint5 = Label(root, text = "Joint 5 Position")
+        #joint5.place(relx=0.75, rely=0.5 + 0.25)
         
-        e_1 = Entry(root)
-        e_1.place(relx=0.825, rely=0.5 + 0.05)
-        e_2 = Entry(root)
-        e_2.place(relx=0.825, rely=0.5 + 0.10)
-        e_3 = Entry(root)
-        e_3.place(relx=0.825, rely=0.5 + 0.15)
-        e_4 = Entry(root)
-        e_4.place(relx=0.825, rely=0.5 + 0.20)
-        e_5 = Entry(root)
-        e_5.place(relx=0.825, rely=0.5 + 0.25)
+        #e_1 = Entry(root)
+        #e_1.place(relx=0.825, rely=0.5 + 0.05)
+        #e_2 = Entry(root)
+        #e_2.place(relx=0.825, rely=0.5 + 0.10)
+        #e_3 = Entry(root)
+        #e_3.place(relx=0.825, rely=0.5 + 0.15)
+        #e_4 = Entry(root)
+        #e_4.place(relx=0.825, rely=0.5 + 0.20)
+        #e_5 = Entry(root)
+        #e_5.place(relx=0.825, rely=0.5 + 0.25)
         
-        joint1data = Button(root, text = "Enter", command = lambda: [jointSend(e_1)], activebackground="green")
-        joint1data.place(relx=0.925, rely = 0.5 + 0.05)
-        joint2data = Button(root, text = "Enter", command = lambda: [jointSend(e_2)], activebackground="green")
-        joint2data.place(relx=0.925, rely = 0.5 + 0.10)
-        joint3data = Button(root, text = "Enter", command = lambda: [jointSend(e_3)], activebackground="green")
-        joint3data.place(relx=0.925, rely = 0.5 + 0.15)
-        joint4data = Button(root, text = "Enter", command = lambda: [jointSend(e_4)], activebackground="green")
-        joint4data.place(relx=0.925, rely = 0.5 + 0.20)
-        joint5data = Button(root, text = "Enter", command = lambda: [jointSend(e_5)], activebackground="green")
-        joint5data.place(relx=0.925, rely = 0.5 + 0.25)
+        #joint1data = Button(root, text = "Enter", command = lambda: [jointSend(e_1)], activebackground="green")
+        #joint1data.place(relx=0.925, rely = 0.5 + 0.05)
+        #joint2data = Button(root, text = "Enter", command = lambda: [jointSend(e_2)], activebackground="green")
+        #joint2data.place(relx=0.925, rely = 0.5 + 0.10)
+        #joint3data = Button(root, text = "Enter", command = lambda: [jointSend(e_3)], activebackground="green")
+        #joint3data.place(relx=0.925, rely = 0.5 + 0.15)
+        #joint4data = Button(root, text = "Enter", command = lambda: [jointSend(e_4)], activebackground="green")
+        #joint4data.place(relx=0.925, rely = 0.5 + 0.20)
+        #joint5data = Button(root, text = "Enter", command = lambda: [jointSend(e_5)], activebackground="green")
+        #joint5data.place(relx=0.925, rely = 0.5 + 0.25)
     
     homeLabel=Label(root,text="GripPi",font=("Times_New_Roman",25), anchor="e", justify=CENTER)
     homeLabel.pack(side=TOP)
@@ -263,11 +268,14 @@ def screenHome():
     powerButton=Button(root,text="POWER",font=("Times_New_Roman",25),command=close_gui,activebackground="red")
     powerButton.pack(side=BOTTOM)
     
-    calibrateButton=Button(root,text="CALIBRATE",font=("Times_New_Roman",25),command = lambda: [screenItemSelect()],background="green",activebackground="red")
+    calibrateButton=Button(root,text="CALIBRATE",font=("Times_New_Roman",25),command = lambda: [screenItemSelect(),calibrate()],background="green",activebackground="red")
     calibrateButton.place(relx=.5,rely=.5,anchor= CENTER)
     
-    motionControlButton=Button(root,text="GripPi Motion Control",font=("Times_New_Roman",25),command=screenMotionControl,background="green",activebackground="red")
-    motionControlButton.place(relx=0.9,rely=.9,anchor= CENTER)
+    #motionControlButton=Button(root,text="GripPi Motion Control",font=("Times_New_Roman",25),command=screenMotionControl,background="green",activebackground="red")
+    #motionControlButton.place(relx=0.9,rely=.9,anchor= CENTER)
+    
+    binsButton=Button(root, text="BINS", font=("Times_New_Roman",25), command = lambda: [screenItemSelect()], background="green",activebackground="red")
+    binsButton.place(relx=.5,rely=0.7,anchor= CENTER)
     
 
 screenHome()
