79eba3c3cf64316a90be2add04737c1ac43f5262
Louis Kraft <99083123+LouEEEg@users.noreply.github.com>
2023-05-01 14:48:24 -0700

    Update GripPi_List_example.c

--- Manipulator/Joint Position Tracking/GripPi_List_example.c
+++ Manipulator/Joint Position Tracking/GripPi_List_example.c
@@ -2,8 +2,14 @@
 
 int main(){
     MyLinkedList *path_head = myLinkedListCreate();
-    pathPlanner(0,0,0,100,100,100);
-    myLinkedListPrint(path_head);
+    void *head = path_head;
+    // myLinkedListAddAtTail(MyLinkedList* obj, int x_val, int y_val, int z_val, int gripper_val)
+    myLinkedListAddAtTail(path_head, 999, 998, 997, 996, 90);
+    
+    //head is the first node in the list. 
+    // to return to the first node call path_head = head
+    // to along the list path_head = path_head->next;
+    // the final node returns a NULL type for path_head->next;
      
     return(0);
 }
